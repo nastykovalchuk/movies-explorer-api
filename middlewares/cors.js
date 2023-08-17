b155ff8736849e3f62e1236c77631430001afec8
nastykovalchuk <kovalchuk.anastasiya.m@yandex.ru>
2023-08-17 15:22:24 +0300

    fix: add cred header for coocies

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,6 +14,7 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
